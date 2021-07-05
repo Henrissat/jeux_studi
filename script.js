f2e30ef47de505b55be431caa0a2b65e38f33ab5
Mathieu <studio.creatic@gmail.com>
2021-07-05 16:34:27 +0200

    fin des ajouts pour le script, btn newGame, btn Hold, newPlayer, Player actif

--- script.js
+++ script.js
@@ -2,34 +2,45 @@ var dice = document.getElementById('dice')
 var rollDiceBtn = document.getElementById('rollDiceBtn')
 var currentClass = ''
 var playerActive, scores, roundScore 
+/*
+var player1 = document.getElementById('player-0-container')
+var player2 = document.getElementById('player-1-container')
+var playerActive = document.querySelector('current-player')
+players = [ player1, player2 ]*/
 
-playerActive = 0;
-scores = [0, 0];
-roundScore = 0;
 
-/*newGame();
-document.getElementById("newGame").addEventListener("click", newGame);
-function newGame{}*/
+document.getElementById('newGame').addEventListener("click", newGame);
+function newGame(){
+  init();
+  scores = [0, 0];
+  roundScore = 0;
+  playerActive = 0;
+}
 
-/* JOUEUR 1 */
+/* JOUEURS */
 /*  Initialiser le score  */ 
-let scoreTotal1 = document.getElementById('scoreTotal-1')
-scoreTotal1.innerText = 0
 
-let roundScore1 = document.getElementById('roundScore-1')
-roundScore1.innerText = 0
+function init(){
+  let scoreTotal1 = document.getElementById('scoreTotal-0')
+  scoreTotal1.innerText = 0
+  let roundScore1 = document.getElementById('roundScore-0')
+  roundScore1.innerText = 0
+  let scoreTotal2 = document.getElementById('scoreTotal-1')
+  scoreTotal2.innerText = 0
+  let roundScore2 = document.getElementById('roundScore-1')
+  roundScore2.innerText = 0
+}
+init();
 
-
+roundScore = 0;
 //fonction au clic pour faire apparaitre la face en fonction de rollDiceRand
 
 rollDiceBtn.addEventListener("click", ()=>{
   //fonction qui génère un nombre entre 1 et 6
   var rollDiceRand = Math.floor(Math.random() * 6) + 1
-  
   var randNum = rollDiceRand;
-  roundScore1.textContent = randNum
 
-  //faire apparaitre la face correspondant au randNum
+  //faire apparaitre la face show- correspondant au randNum
   var showClass = 'show-' + randNum
   if ( currentClass ) {
     dice.classList.remove( currentClass )
@@ -37,11 +48,12 @@ rollDiceBtn.addEventListener("click", ()=>{
   dice.classList.add( showClass )
   currentClass = showClass
 
-  // stocker le score ou Game Over
+  // additionner le score ou Game Over
   if (rollDiceRand >= 2) {
     roundScore += rollDiceRand
     document.getElementById('roundScore-'+ playerActive).innerText = roundScore
-  } else if {
+  } else if(rollDiceRand == 1) {
+    roundScore = 0
     document.getElementById('roundScore-'+ playerActive).innerText = "Ooops"
     nextPlayer();
   } else {
@@ -49,27 +61,43 @@ rollDiceBtn.addEventListener("click", ()=>{
   }
 }) 
 
+/*    Joueur suivant et joueur actif   */
+playerActive = 0;
 function nextPlayer(){
-
-
+  if(playerActive === 0){
+    playerActive = 1;
+  } else {
+    playerActive = 0;
+  }
+  roundScore = 0
+  document.getElementById('roundScore-'+playerActive).innerText = 0
 }
 
-console.log()
-/*while (resultDice >= 2){
-  roundScore1 = roundScore1 + resultDice
-}*/
+/*    stocker le score   */
+scores = [0, 0];
+holdBtn.addEventListener("click", ()=>{
+  scores[playerActive] += roundScore
+  document.getElementById('scoreTotal-'+playerActive).innerText = scores[playerActive]
+  console.log(scores)
+  winner();
+  nextPlayer()
+})
 
+function winner(){
+  if(scores[playerActive] >= 100){
+    window.alert('YOU WIN')
+  }
+}
 
 //créer mon dé
 //visuel du dé de 6
-//const roll = document.getElementById('#roll'){};
 
 /*créer l'objet dé avec canvas et l'animer
 var canvas, ctx;
 var diceColor = '#fff'
 var pointColor = 'crimson'
 
-const canvas = document.getElementById('roll')
+const canvas = document.getElementById('dice')
 const ctx = canvas.getContext('2D')
 canvas.width = 500
 canvas.height = 500
