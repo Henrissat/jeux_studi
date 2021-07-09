3922e0658e115ae0732f55cd343d836e28c147fc
Mathieu <studio.creatic@gmail.com>
2021-07-09 18:48:47 +0200

    fin du graphisme

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ var player2 = document.getElementById('player-1-container')
 var playerActive = document.querySelector('current-player')
 players = [ player1, player2 ]*/
 
+/* initialise musique ambiance */
 var soundGame = document.getElementById('soundGame')
 soundGame.volume = 0.30
+document.getElementById('soundGameCut').addEventListener("click", ()=>{
+  soundGame.pause()
+})
 
+/* new game */
 document.getElementById('newGame').addEventListener("click", newGame);
 function newGame(){
   init()
@@ -46,7 +51,7 @@ rollDiceBtn.addEventListener("click", ()=>{
   //fonction qui génère un nombre entre 1 et 6
   var rollDiceRand = Math.floor(Math.random() * 6) + 1
   var randNum = rollDiceRand;
-
+  
   //faire apparaitre la face show- correspondant au randNum
   var showClass = 'show-' + randNum
   if ( currentClass ) {
@@ -54,6 +59,31 @@ rollDiceBtn.addEventListener("click", ()=>{
   }
   dice.classList.add( showClass )
   currentClass = showClass
+
+  console.log(randNum)
+  /* Afficher le dé graphique correspondant à RollDice  
+  switch (currentClass) {
+    case 1:
+      console.log('afficher le dés de 1');
+      break;
+    case 2:
+      console.log('afficher le dés de 2');
+      break;
+    case 3:
+      console.log('afficher le dés de 3');
+      break;
+    case 4:
+      console.log('afficher le dés de 4');
+      break;
+    case 5:
+      console.log('afficher le dés de 5');
+      break;
+    case 6:
+      console.log('afficher le dés de 6');
+      break;
+    default:
+      console.log(`Sorry, we are out of ${randNum}.`);
+  }*/
 
   // additionner le score ou Game Over
   if (rollDiceRand >= 2) {
@@ -69,7 +99,8 @@ rollDiceBtn.addEventListener("click", ()=>{
   } else {
     document.getElementById('roundScore-'+ playerActive).innerText = "Erreur"
   }
-}) 
+})
+
 
 /*   ----- Joueur suivant et joueur actif -----  */
 playerActive = 0;
@@ -103,7 +134,6 @@ function winner(){
     window.alert('YOU WIN')
   }
 }
-
 /*  -----  EFFETS  -----  */
 /*function songByPlayer(){
   if (playerActive = 0 ){
@@ -119,8 +149,10 @@ function winner(){
   } else if (playerActive = document.getElementById('player-1-container')){
     document.getElementById('rollSong2').play()
   }
-}*/
+}
 console.log(playerActive)
+*/
+
 
 
 /*// Son Dés Aléatoire 
@@ -151,6 +183,14 @@ function textGameOver(){
   var text = ['Ooops','You loose','AH AH AH suivant']
   document.getElementById('roundScore-'+ playerActive).innerText = text[Math.floor(Math.random() * text.length)]
 }
+
+
+/*Avatar()
+function Avatar(){
+  document.getElementById('avatar-'+ playerActive).innerHTML = '<div id="avatar-'+playerActive'"></div>'
+}*/
+
+
 // Son newGame aléatoire
 function newGameSong(){
   var song = ['are-you-ready.mp3','are-you-ready-to-go.mp3','are-you-ready-to-win.mp3']
@@ -167,38 +207,94 @@ function newGameSong(){
 //créer mon dé
 //visuel du dé de 6
 
-/*créer l'objet dé avec canvas et l'animer
-var canvas, ctx;
-var diceColor = '#fff'
-var pointColor = 'crimson'
+/*  créer l'objet dé avec canvas et l'animer
+var creatCanvas = document.getElementById('cadre').innerHTML('<canvas class="cornerCadre-top-left"></canvas>');
+creatCanvas;
+function dice5(){
+  const canvas = document.getElementById('dice');
+  canvas.width = 200
+  canvas.height = 200
 
-const canvas = document.getElementById('dice')
-const ctx = canvas.getContext('2D')
-canvas.width = 500
-canvas.height = 500
+  let ctx;
+  if (canvas.getContext) {
 
-drawDice*/
+    ctx = canvas.getContext('2d') ;
+    const lingrad = ctx.createLinearGradient(0, 0, 0, 150);
+    lingrad.addColorStop(1, 'rgba(218, 215, 209, 0.8)');
+    lingrad.addColorStop(0, 'white');
 
-/*Afficher le dé graphique correspondant à RollDice
-switch (result) {
-case 1:
-  console.log('afficher le dés de 1');
-  break;
-case 2:
-  console.log('afficher le dés de 2');
-  break;
-case 3:
-  console.log('afficher le dés de 3');
-  break;
-case 4:
-  console.log('afficher le dés de 4');
-  break;
-case 5:
-  console.log('afficher le dés de 5');
-  break;
-case 6:
-  console.log('afficher le dés de 6');
-  break;
-default:
-  console.log(`Sorry, we are out of ${result}.`);
-}*/
+    ctx.fillStyle = lingrad;
+    ctx.fillRect(0, 0, 200, 200);
+
+  // arc plein
+  ctx.beginPath();
+  ctx.fillStyle = 'rgba(215, 50, 40, 1)';
+  ctx.arc(50, 50, 20, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(50, 150, 20, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(150, 50, 20, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(150, 150, 20, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, Math.PI * 2);
+  ctx.fill();
+  }
+}
+*/
+
+
+/* ---- Background effets brazier  ---- */
+(function(){
+	
+	var c = document.getElementById("bgEffects"),
+			ctx = c.getContext("2d");
+	
+	c.width = innerWidth;
+	c.height = innerHeight;
+	
+	var lines = [],
+			maxSpeed = 5,
+			spacing = 5,
+			xSpacing = 0,
+			n = innerWidth / spacing,
+			colors = ["rgba(224, 137, 55, 0.)", "rgba(240, 89, 44, 0.4)", "rgba(255, 206, 73, 0.3)", "rgba(255, 236, 183, 0.2)"],
+			i;
+	
+	for (i = 0; i < n; i++){
+		xSpacing += spacing;
+		lines.push({
+			x: xSpacing,
+			y: Math.round(Math.random()*c.height),
+			width: 2,
+			height: Math.round(Math.random()*(innerHeight/10)),
+			speed: Math.random()*maxSpeed + 3,
+			color: colors[Math.floor(Math.random() * colors.length)]
+		});
+	}
+	
+	
+	function draw(){
+		var i;
+		ctx.clearRect(0,0,c.width,c.height);
+		
+		for (i = 0; i < n; i++){
+			ctx.fillStyle = lines[i].color;
+			ctx.fillRect(lines[i].x, lines[i].y, lines[i].width, lines[i].height);
+			lines[i].y += lines[i].speed;
+			
+			if (lines[i].y > c.height)
+				lines[i].y = 0 - lines[i].height;
+		}
+		
+		requestAnimationFrame(draw);
+		
+	}
+	
+	draw();
+	
+}());
